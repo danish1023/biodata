ccd889e8cd69e1af2a3456bb4032719a478cfe69
danish1023 <danish1023@gmail.com>
2018-05-08 11:30:33 +0530

    PWA

--- lite/main.js
+++ lite/main.js
@@ -1,6 +1,6 @@
 (function () {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./service-worker.js').then(function (registration) {
+    navigator.serviceWorker.register('./lite/service-worker.js').then(function (registration) {
       console.log('Registration successful, scope is:', registration.scope);
     }).catch(function (error) {
       console.log('Registration failed:', error);
